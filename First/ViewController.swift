7fd2b8dc330019cd3a16612ad9071712d828b1c4
user113228 <user113228@mac090.local>
2015-11-20 00:04:24 -0500

    Initial Commit

---
+++ First/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by user113228 on 11/20/15.
+//  Copyright © 2015 user113228. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
